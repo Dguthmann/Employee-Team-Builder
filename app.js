325b2d08ed40c97d5055f27abbb18883fab8f335
David Guthmann <drguthmann@cox.net>
2020-10-05 23:36:14 -0500

    testing writing the file, works

--- app.js
+++ app.js
@@ -59,15 +59,22 @@ function buildTeam() {
                 break;
 
             default:
+                // May need a call to if no one is on team, force edge case of add someone
                 console.log("Team Added to board")
                 console.table(myEmployees);
                 // call rendering using myEmployees Array
-                // render(myEmployees);
+                let myTeamHTML = render(myEmployees);
+                fs.writeFile('./output/team.html', myTeamHTML, (err) => {
+                    if(err) throw err;
+                    console.log("File Saved");
+                });
                 break;
         }
     })
 }
 
+
+// make a function that makes inquirer call to generate a new Manager Employee
 function createManager() {
         inquirer.prompt([
             {
@@ -84,10 +91,7 @@ function createManager() {
                 name: "officePhone"
             }
         ]).then(function (answers) {
-            // Set a placeholder variable
-            let selectedManager;
-            // Use placeholder to create new intern object
-            // selectedManager.addManager(answers.name, idHolder, answers.email, answers.officePhone);
+           // Take answers and make new Manager object, push it into the myEmployees Array
             myEmployees.push(new Manager(answers.name, idHolder, answers.email, answers.officePhone));
             // increment for new id number for next employee
             idHolder++;
@@ -95,6 +99,7 @@ function createManager() {
         })
 }
 
+// a function that makes inquirer call to generate a new Engineer Employee
 function createEngineer() {
     inquirer.prompt([
         {
@@ -111,18 +116,15 @@ function createEngineer() {
             name: "github"
         }
     ]).then(function (answers) {
-        // Set a placeholder variable
-        let selectedEngineer;
-        // Use placeholder to create new intern object
-        // selectedEngineer.addEngineer(answers.name, idHolder, answers.email, answers.github);
+        // Take answers and make new Engineer object, push it into the myEmployees Array
         myEmployees.push(new Engineer(answers.name, idHolder, answers.email, answers.github));
-        // building the new 
         // increment for new id number for next employee
         idHolder++;
         buildTeam();
     })
 }
 
+// a function that makes inquirer call to generate a new Intern Employee
 function createIntern() {
     inquirer.prompt([
         {
@@ -139,10 +141,7 @@ function createIntern() {
             name: "school"
         }
     ]).then(function (answers) {
-        // Set a placeholder variable
-        let selectedIntern;
-        // Use placeholder to create new intern object
-        // selectedIntern.addIntern(answers.name, idHolder, answers.email, answers.school);
+        // Take answers and make new Intern object, push it into the myEmployees Array
         myEmployees.push(new Intern(answers.name, idHolder, answers.email, answers.school));
         // increment for new id number for next employee
         idHolder++;
