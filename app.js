0f8fb7c21b5da930045ae29ba543a043737c5edf
David Guthmann <drguthmann@cox.net>
2020-10-05 20:47:56 -0500

    Create Employee Object Constructor function

--- app.js
+++ app.js
@@ -33,3 +33,6 @@ const render = require("./lib/htmlRenderer");
 // for further information. Be sure to test out each class and verify it generates an
 // object with the correct structure and methods. This structure will be crucial in order
 // for the provided `render` function to work! ```
+
+
+// Add a function that calls upon start, this function leads to the other options for a new employee
